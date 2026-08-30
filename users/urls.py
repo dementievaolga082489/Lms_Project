28ef16820d5df25efcfea3cd3d8af4600a0dad30
Olga Dementieva <08289@mail.ru>
2026-08-30 13:40:40 +0300

    изменения в urls.py users

--- users/urls.py
+++ users/urls.py
@@ -1,17 +1,22 @@
 from django.urls import path
+from rest_framework.decorators import permission_classes
+from rest_framework.permissions import AllowAny
 from rest_framework.routers import SimpleRouter
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from users.apps import UsersConfig
-from users.views import PaymentListView, UserProfileViewSet
+from users.views import PaymentListView, UserProfileViewSet, UserCreateAPIView, UserViewSet
 
 app_name = UsersConfig.name
 
 router = SimpleRouter()
-router.register("users", UserProfileViewSet)
+router.register("users", UserViewSet, basename="user")
 
 
 urlpatterns = [
-
+    path('register/', UserCreateAPIView.as_view(), name='register'),
     path('payments/', PaymentListView.as_view(), name='payment-list'),
+    path('login/', TokenObtainPairView.as_view(permission_classes=(AllowAny,)), name='login'),
+    path('token/refresh/', TokenRefreshView.as_view(permission_classes=(AllowAny,)), name='token_refresh'),
    ]
 urlpatterns += router.urls
