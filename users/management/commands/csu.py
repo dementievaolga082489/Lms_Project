3e7ad12fb0dfae8d61f83042005d68a1c3b66c75
Olga Dementieva <08289@mail.ru>
2026-08-30 13:45:08 +0300

    изменения в csu.py

--- users/management/commands/csu.py
+++ users/management/commands/csu.py
@@ -5,7 +5,7 @@ from users.models import User
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        user = User.objects.create(email="admin@admin.com")
+        user = User.objects.create(email="admin1@admin.com")
         user.set_password("19AaZz")
         user.is_active = True
         user.is_staff = True
