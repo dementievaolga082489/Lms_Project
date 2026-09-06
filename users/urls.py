e61affc0850efbcd719fb4dcc0f46086c132952a
Olga Dementieva <08289@mail.ru>
2026-09-06 14:57:33 +0300

    urls.py users

--- users/urls.py
+++ users/urls.py
@@ -2,10 +2,13 @@ from django.urls import path
 from rest_framework.decorators import permission_classes
 from rest_framework.permissions import AllowAny
 from rest_framework.routers import SimpleRouter
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework_simplejwt.views import (TokenObtainPairView,
+                                            TokenRefreshView)
 
 from users.apps import UsersConfig
-from users.views import PaymentListView, UserCreateAPIView, UserViewSet, SubscriptionView, UserSubscriptionListView
+from users.views import (PaymentCreateApiView, PaymentListView,
+                         SubscriptionView, UserCreateAPIView,
+                         UserSubscriptionListView, UserViewSet)
 
 app_name = UsersConfig.name
 
@@ -14,12 +17,24 @@ router.register("users", UserViewSet, basename="user")
 
 
 urlpatterns = [
-    path('register/', UserCreateAPIView.as_view(), name='register'),
-    path('payments/', PaymentListView.as_view(), name='payment-list'),
-    path('login/', TokenObtainPairView.as_view(permission_classes=(AllowAny,)), name='login'),
-    path('token/refresh/', TokenRefreshView.as_view(permission_classes=(AllowAny,)), name='token_refresh'),
-    path('subscriptions/toggle/', SubscriptionView.as_view(), name='subscription-toggle'),
-    path('subscriptions/my/', UserSubscriptionListView.as_view(), name='my-subscriptions'),
-
+    path("register/", UserCreateAPIView.as_view(), name="register"),
+    path("payments/", PaymentListView.as_view(), name="payment-list"),
+    path(
+        "login/",
+        TokenObtainPairView.as_view(permission_classes=(AllowAny,)),
+        name="login",
+    ),
+    path(
+        "token/refresh/",
+        TokenRefreshView.as_view(permission_classes=(AllowAny,)),
+        name="token_refresh",
+    ),
+    path(
+        "subscriptions/toggle/", SubscriptionView.as_view(), name="subscription-toggle"
+    ),
+    path(
+        "subscriptions/my/", UserSubscriptionListView.as_view(), name="my-subscriptions"
+    ),
+    path("payments/stripe/", PaymentCreateApiView.as_view(), name="payment"),
 ]
 urlpatterns += router.urls
